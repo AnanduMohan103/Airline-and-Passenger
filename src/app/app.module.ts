8dfcbbe8f8b610840acb33e967c6034e53ca54ff
AnanduMohan103 <anandhumohan103@gmail.com>
2022-10-11 22:14:07 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AirlineComponent } from './airline/airline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AirlineComponent
   ],
   imports: [
     BrowserModule,
